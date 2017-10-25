cc22ef012764347d1bfc35eae4dd3ec10ca8756c
Michael Seib <Seib_Michael_J@cat.com>
2017-10-25 14:41:54 -0500

    VSTS to Github

--- src/components/SingleTodo.js
+++ src/components/SingleTodo.js
@@ -1,22 +1,22 @@
 import React from 'react';
-//Test
+//Test VSTS
 export default class SingleTodo extends React.Component {
     render() {
-        return (
-            <li>
-                <input
-                    data-id={this.props.todoId}
-                    checked={this.props.isDone}
-                    onChange={this.props.archiveToggleTodo}
-                    type="checkbox"
-                />
-                <label>{this.props.text}{this.props.isDone? " - DONE": ""}</label>
-                <button
-                    data-id={this.props.todoId}
-                    onClick={this.props.removeTodo}>
-                        Remove
-                </button>
-            </li>
+        return ( <
+            li >
+            <
+            input data - id = { this.props.todoId }
+            checked = { this.props.isDone }
+            onChange = { this.props.archiveToggleTodo }
+            type = "checkbox" /
+            >
+            <
+            label > { this.props.text } { this.props.isDone ? " - DONE" : "" } < /label> <
+            button data - id = { this.props.todoId }
+            onClick = { this.props.removeTodo } >
+            Remove <
+            /button> <
+            /li>
         );
     }
 }
